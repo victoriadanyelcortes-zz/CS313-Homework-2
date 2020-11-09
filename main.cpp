5e016219d5461994282a0d12413425f8b5988c7e
Victoria Cortes <vickydanyel.vc@gmail.com>
2020-11-09 12:55:23 -0500

    Add comments

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include <string>
 #include "Stack.h"
 
-
+// evaluate prefix expressions
 double eval_prefix(std::string exp) {
-
+    // instantiate stack and create variable for
+    // operations
     Stack<double> stack;
     double op1, op2;
-
+    // iterate thru the expression starting at the top
     for(int i = exp.size()-1; i >= 0; i--) {
+        // check if it's a digit or operator
+        // push to stack if digit
         if(isdigit(exp[i])) {
             stack.push(exp[i] - '0');
         }
@@ -16,7 +19,7 @@ double eval_prefix(std::string exp) {
             op1 = stack.top();
             stack.pop();
 
-            if(stack.check_empty()) {
+            if(stack.check_empty()){
                 std::cout << "No operations"  << std::endl;
             }
             else {
@@ -25,21 +28,26 @@ double eval_prefix(std::string exp) {
 
                 switch (exp[i]) {
                     case '+':
+                        // push op1 + op2
                         stack.push(op1 + op2);
                         break;
                     case '-':
+                        // push op1 - op2
                         stack.push(op1 - op2);
                         break;
                     case '*':
+                        // push op1 * op2
                         stack.push(op1 * op2);
                         break;
                     case '/':
+                        // push op1 / op2
                         stack.push(op1 / op2);
                         break;
                 }
             }
         }
     }
+    // return the result stored in stack top
     return stack.top();
 }
 
@@ -63,15 +71,18 @@ int main(int argc, char** argv) {
     // using function to list all elements in the stack
     // using function to change element in stack based on its position
     std::cout << "Question 4: " << std::endl;
+    // allow user to choose stack size
     int size;
     std::cout << "What size do you want the stack to be? ";
     std::cin >> size;
     Stack<int> stack_nums(size);
 
+    // push every number from one to the size +1
     for(int i = 1; i < size+1; ++i) {
         stack_nums.push(i);
     }
 
+    // list all elements and show the changed value
     stack_nums.list_all_elements();
     stack_nums.change_element_value();
 
